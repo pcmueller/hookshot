79fa441795aea1aa40a05d2337aaf98de163f581
pcmueller <pmuellerleile@gmail.com>
2021-06-09 10:27:22 -0600

    Convert App to class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,25 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      location: '',
+      category: '',
+      item: '',
+    }
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <header className='header'>
+          <h1 className='title'>HOOKSHOT</h1>
+          
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
