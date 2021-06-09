82f2eb0c5214cbc36ddf303c44d09ad3927fc993
pcmueller <pmuellerleile@gmail.com>
2021-06-09 13:25:21 -0600

    Replace browser tab icon

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -2,13 +2,13 @@ const baseURL = 'https://botw-compendium.herokuapp.com/api/v2';
 
 const apiCalls = {
 
-  fetchAllData = async () => {
+  async fetchAllData() {
     const response = await fetch(`${baseURL}`);
     const data = await response.json();
     return data;
   },
 
-  fetchDataByCategory = async (category) => {
+  async fetchDataByCategory(category) {
     const response = await fetch(`${baseURL}/category${category}`);
     const data = await response.json();
     return data;
