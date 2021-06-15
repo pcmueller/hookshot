7c721c0818e40fe8195be816728c1ea828d060d0
pcmueller <pmuellerleile@gmail.com>
2021-06-14 21:01:36 -0600

    Finish responsiveness refactoring

---
+++ src/components/Search/Search.js
@@ -0,0 +1,18 @@
+{/* <div className='goal-heading'>
+  <h4>~OR~</h4>
+</div> */}
+
+
+{/* <section className='search-section nes-container is-rounded'>
+  <label className='search-label' htmlFor="search_field">looking for an item or creature?</label>
+  <div className='nes-field search-bar'>
+    <input
+      type='text'
+      id='search_field'
+      className='nes-input is-inline search-input'
+      placeholder='search here!'
+      name='input'
+    />
+    <button type='submit' className='nes-btn'>search</button>
+  </div>
+</section> */}
